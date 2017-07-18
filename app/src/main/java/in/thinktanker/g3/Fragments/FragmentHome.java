68f685e8bdc5c7fd589c611df9e7449a45e94c63
megha <sanket.thinktanker@gmail.com>
2017-07-18 14:41:14 +0530

    commited

---
+++ app/src/main/java/in/thinktanker/g3/Fragments/FragmentHome.java
@@ -0,0 +1,30 @@
+package in.thinktanker.g3.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import in.thinktanker.g3.R;
+
+/**
+ * Created by MY PC on 18-Jul-17.
+ */
+public class FragmentHome extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        init(view);
+
+
+        return view;
+    }
+
+    private void init(View view) {
+
+    }
+}
